2fa90b256c44f75ea1264f5df9ef695a2dce491a
Daniel Gempesaw <gempesaw@gmail.com>
2015-12-09 11:56:44 -0500

    initial commit

---
+++ lib/Honeydew/Gmail.pm
@@ -0,0 +1,15 @@
+package Honeydew::Gmail;
+
+# ABSTRACT:
+use strict;
+use warnings;
+
+=for markdown [![Build Status](https://travis-ci.org/honeydew-sc/Honeydew-Gmail.svg?branch=master)](https://travis-ci.org/honeydew-sc/Honeydew-Gmail)
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+1;
